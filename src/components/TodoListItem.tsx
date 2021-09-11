8ddc7e0b5e2b969770caede3608c9e0f1df7488f
David Wu <david.wu@zerologix.com>
2021-09-11 19:08:34 +0800

    feature: create TodolistItem component

---
+++ src/components/TodoListItem.tsx
@@ -0,0 +1,63 @@
+import React, { ChangeEvent } from 'react';
+import { Todo } from 'src/store/todoList';
+import styled from 'styled-components';
+
+export interface TodoListItemProps {
+  todo: Todo
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid palevioletred;
+  border-radius: 8px;
+  margin-bottom: 4px;
+  padding: 0 4px;
+`;
+const Title = styled.h3`
+  font-size: 1.5rem;
+  color: #727f8b;
+`;
+const TagsWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Tag = styled.span`
+  background-color: CornflowerBlue;
+  border-radius: 4px;
+  color: white;
+  padding: 4px 6px;
+  margin: 4px;
+`;
+const CheckLabel = styled.span`
+  color: #727f8b;
+  font-weight: bolder;
+`;
+
+const TodoListItem = (props: TodoListItemProps): React.ReactElement => {
+  const { todo } = props;
+  const tags = todo.tags.map((tag) => <Tag key={tag}>{tag}</Tag>);
+  const handleCompletedChange = (event: ChangeEvent<HTMLInputElement>) => {
+    // TODO: implement toggle completed feature
+  };
+  return (
+    <Wrapper>
+      <Title>
+        {todo.title}
+      </Title>
+      <TagsWrapper>
+        {tags}
+      </TagsWrapper>
+      <div>
+        <CheckLabel>
+          Completed
+          <input type="checkbox" checked={todo.completed} onChange={handleCompletedChange} />
+        </CheckLabel>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default TodoListItem;
