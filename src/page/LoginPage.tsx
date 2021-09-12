c1501fa2b66c2ae98a388adc4163a4bc4ec00050
David Wu <david.wu@zerologix.com>
2021-09-12 10:11:39 +0800

    feature: implement login page

--- src/page/LoginPage.tsx
+++ src/page/LoginPage.tsx
@@ -1,9 +1,74 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import useUserStore from 'src/store/user';
+import styled from 'styled-components';
+import { Location } from 'history';
 
-const LoginPage = (): React.ReactElement => (
-  <div>
-    Login Page
-  </div>
-);
+type LocationState = {
+  from: Location;
+};
+
+const Wrapper = styled.div`
+  max-width: 300px;
+  margin: 60px auto;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 16px 0;
+`;
+
+const InputLabel = styled.span`
+  font-weight: bolder;
+  margin-right: 10px;
+  width: 80px;
+  display: inline-block;
+`;
+const Input = styled.input`
+  flex: 1;
+`;
+
+const Button = styled.button`
+  color: black;
+  font-weight: bolder;
+  background-color: white;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+`;
+
+const LoginPage = (): React.ReactElement => {
+  const setUserId = useUserStore((state) => state.setId);
+  const history = useHistory();
+  const location: Location<LocationState> = useLocation();
+
+  const handleLogin = () => {
+    // TODO: implement login api
+    setUserId(1234);
+    const { from } = location.state;
+    const nextPath = from.pathname || '/';
+    history.push(nextPath);
+  };
+
+  return (
+    <Wrapper>
+      <InputWrapper>
+        <InputLabel>
+          Account
+        </InputLabel>
+        <Input value="IamDavid" readOnly />
+      </InputWrapper>
+      <InputWrapper>
+        <InputLabel>
+          Password
+        </InputLabel>
+        <Input value="123456" type="password" readOnly />
+      </InputWrapper>
+      <Button onClick={handleLogin}>Login</Button>
+    </Wrapper>
+  );
+};
 
 export default LoginPage;
