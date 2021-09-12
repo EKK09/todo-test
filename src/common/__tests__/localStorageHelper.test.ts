e328f2b7aed2b3f4b6e2d94a5f3b2e82c98e2bcd
David Wu <david.wu@zerologix.com>
2021-09-12 16:20:15 +0800

    test: modify localStorage test

--- src/common/__tests__/localStorageHelper.test.ts
+++ src/common/__tests__/localStorageHelper.test.ts
@@ -3,19 +3,16 @@ import { getToken, setToken, removeToken } from '../localStorageHelper';
 describe('localStorageHelper', () => {
   const token = 'token';
   it('get token', () => {
-    const spy = jest.spyOn(window.localStorage, 'getItem');
     getToken();
-    expect(spy).toBeCalledWith(token);
+    expect(localStorage.getItem).toBeCalledWith(token);
   });
   it('set token', () => {
     const fooToken = 'fooToken';
-    const spy = jest.spyOn(window.localStorage, 'setItem');
     setToken(fooToken);
-    expect(spy).toBeCalledWith(token, fooToken);
+    expect(localStorage.setItem).toBeCalledWith(token, fooToken);
   });
   it('remove token', () => {
-    const spy = jest.spyOn(window.localStorage, 'removeItem');
     removeToken();
-    expect(spy).toBeCalledWith(token);
+    expect(localStorage.removeItem).toBeCalledWith(token);
   });
 });
