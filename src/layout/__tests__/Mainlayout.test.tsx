1512f40a96048354d5b4b14ce16bcffccf29da55
David Wu <david.wu@zerologix.com>
2021-09-12 17:28:16 +0800

    test: fix Mainlayout test

--- src/layout/__tests__/Mainlayout.test.tsx
+++ src/layout/__tests__/Mainlayout.test.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import MainLayout from '../MainLayout';
 
+jest.mock('src/components/Header', () => 'Mock-Header');
+jest.mock('src/route/RouteView', () => 'Mock-RouterView');
+
 describe('Mainlayout component', () => {
-  it('Render Mainlayout', () => {
+  it('Render Mainlayout', async () => {
     const wrapper = render(<MainLayout />);
+
     expect(wrapper.container).toMatchSnapshot();
   });
 });
